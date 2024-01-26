3666b8c7241426a1802ba13ddf2157cd846829ed
qolvap <pgrebennikow@yahoo.pl>
2024-01-26 16:10:11 +0100

    ✅ extension set up, cleared pages, edited footer date, edited resultpage, edited fuse search

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
-import HomePage from './components/Home/HomePage';
 import ResultsPage from './components/Results/ResultsPage';
-import ContactPage from './components/Contact/ContactPage';
 import Footer from './components/Footer/Footer';
-import { AppProvider } from './components/context/Context';
 import 'tailwindcss/tailwind.css';
 
 function App() {
   return (
-    <Router>
-      <AppProvider>
         <div className="App flex flex-col min-h-screen">
           <div className="flex-grow flex-shrink-0">
-            <Sidebar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/Results" element={<ResultsPage />} />
-              <Route path="/Contact" element={<ContactPage />} />
-            </Routes>
+              <ResultsPage />
           </div>
           <Footer />
         </div>
-      </AppProvider>
-    </Router>
   );
 }
 
--- src/components/Results/ResultsPage.tsx
+++ src/components/Results/ResultsPage.tsx
@@ -1,9 +1,19 @@
 import React, { useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
 import { Fine, data } from '../data/data';
 import 'tailwindcss/tailwind.css';
 import Button from '../Button/Button';
 import Fuse from 'fuse.js';
+
+const content = [
+  {
+    text: 'Źródło: UOTD',
+    link: 'https://isap.sejm.gov.pl/isap.nsf/download.xsp/WDU20011251371/U/D20011371Lj.pdf',
+  },
+  { text: 'Legenda: ' },
+  { text: 'PN poważne naruszenie' },
+  { text: 'BPN bardzo poważne naruszenie' },
+  { text: 'NN najpoważniejsze naruszenie' },
+];
 
 // Function to replace Polish characters and convert a string to lowercase
 function podmien(napis: string): string {
@@ -44,7 +54,7 @@ function ResultsPage() {
     const fuse = new Fuse(filteredGuiltyData(), {
       keys: ['description', 'guilty'],
       ignoreLocation: true,
-      threshold: 0.3,
+      threshold: 0.1,
     });
 
     // Perform the search using the lowercased and normalized term
@@ -81,90 +91,106 @@ function ResultsPage() {
   const termLower = podmien(term);
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-between">
+    <div className="flex flex-col min-h-screen  items-center justify-between">
       <div className="mb-4">
       <div className="flex items-center flex-col m-5">
-            <input
-              type="text"
-              placeholder="Wpisz hasło..."
-              value={term}
-              onChange={handleInputChange}
-              onKeyDown={handleEnterKeyPress}
-              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-slate-50 mb-2"
-            />
-            <div className="flex">
-              <select
-                id="guilty"
-                name="guilty"
-                className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm mr-2"
-                value={selectedGuilty}
-                onChange={(e) => setSelectedGuilty(e.target.value)}
-              >
-                <option value="">Taryfikator</option>
-                <option value="kierowcy">Taryfikator Kierowcy</option>
-                <option value="zarządzający">Taryfikator Zarządzającego</option>
-                <option value="przedsiębiorcy">Taryfikator Przedsiębiorcy</option>
-              </select>
-              <Button />
-              <button
-                onClick={handleFilterButtonClick}
-                className="common-button-style"
-              >
-                Wyszukaj
-              </button>
+        <div className="my-12 flex flex-col text-center items-center justify-center">
+          <h2 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Cały taryfikator w jednym miejscu
+          </h2>
+          <h4 className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+            Przejrzyj wykaz naruszeń i wysokość kar
+          </h4>
+        </div>
+        <input
+          type="text"
+          placeholder="Wpisz hasło..."
+          value={term}
+          onChange={handleInputChange}
+          onKeyDown={handleEnterKeyPress}
+          className="block w-full rounded-md border-0 py-1.5 pl-7 pr-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-slate-50 mb-2 max-w-[480px]"
+        />
+        <div className="flex">
+          <select
+            id="guilty"
+            name="guilty"
+            className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm mr-2"
+            value={selectedGuilty}
+            onChange={(e) => setSelectedGuilty(e.target.value)}
+          >
+            <option value="">Taryfikator</option>
+            <option value="kierowcy">Taryfikator Kierowcy</option>
+            <option value="zarządzający">Taryfikator Zarządzającego</option>
+            <option value="przedsiębiorcy">Taryfikator Przedsiębiorcy</option>
+          </select>
+          <Button />
+          <button
+            onClick={handleFilterButtonClick}
+            className="common-button-style"
+          >
+            Wyszukaj
+          </button>
+        </div>
+      </div>
+      {(filteredData.length > 0 || !filteredGuiltyData().some(fine => !fine.description?.includes(term))) ? (
+        <div className="mt-10 grid gap-4 place-items-center m-5">
+          {filteredData.map((fine) => (
+            <div
+              key={fine.number}
+              className="border border-blue-300 rounded-md p-4 max-w-[480px] bg-slate-50 shadow hover:shadow-lg"
+            >
+              <h3 className="text-xl font-semibold mb-2">
+                Taryfikator {fine.guilty}
+              </h3>
+              <p>
+                <span className="font-semibold">Załącznik:</span> {fine.attachment}
+              </p>
+              <p>
+                <span className="font-semibold">Numer w taryfikatorze:</span>{' '}
+                {fine.number}
+              </p>
+              <p>
+                <span className="font-semibold">Opis:</span> {fine.description}
+              </p>
+              <p>
+                <span className="font-semibold">Grzywna:</span> {fine.fine}zł
+              </p>
+              <p>
+                <span className="font-semibold">Poziom naruszenia:</span>{' '}
+                {Array.isArray(fine.group)
+                  ? fine.group.join(', ')
+                  : fine.group}
+              </p>
             </div>
-          </div>
-
-
-        {(filteredData.length > 0 || !filteredGuiltyData().some(fine => !fine.description?.includes(term))) ? (
-          <div className="mt-10 grid gap-4 place-items-center m-5">
-            {filteredData.map((fine) => (
-              <div
-                key={fine.number}
-                className="border border-blue-300 rounded-md p-4 max-w-[480px] bg-slate-50 shadow hover:shadow-lg"
-              >
-                <h3 className="text-xl font-semibold mb-2">
-                  Taryfikator {fine.guilty}
-                </h3>
-                <p>
-                  <span className="font-semibold">Załącznik:</span> {fine.attachment}
-                </p>
-                <p>
-                  <span className="font-semibold">Numer w taryfikatorze:</span>{' '}
-                  {fine.number}
-                </p>
-                <p>
-                  <span className="font-semibold">Opis:</span> {fine.description}
-                </p>
-                <p>
-                  <span className="font-semibold">Grzywna:</span> {fine.fine}zł
-                </p>
-                <p>
-                  <span className="font-semibold">Poziom naruszenia:</span>{' '}
-                  {Array.isArray(fine.group)
-                    ? fine.group.join(', ')
-                    : fine.group}
-                </p>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p className="mt-10 flex flex-col items-center bg-transparent py-0 pl-2 pr-7 text-gray-500">
-            Nie znaleziono
-          </p>
-        )}
+          ))}
+        </div>
+      ) : (
+        <p className="mt-10 flex flex-col items-center bg-transparent py-0 pl-2 pr-7 text-gray-500">
+          Nie znaleziono
+        </p>
+      )}
       </div>
 
       <div className="flex flex-col items-center">
-        <p className="bg-transparent py-0 pl-2 pr-7 text-gray-500">Źródło: UOTD</p>
-        <p className="bg-transparent py-0 pl-2 pr-7 text-gray-500">
-          Legenda: - PN poważne naruszenie - BPN bardzo poważne naruszenie - NN
-          najpoważniejsze naruszenie
-        </p>
-        <br />
-        <Link to="/" className="mr-4 hover:text-blue-600 mb-5">
-          Powrót do strony głównej
-        </Link>
+        {content.map((item, index) => (
+          <p
+            key={index}
+            className="bg-transparent py-0 pl-2 pr-7 text-gray-500"
+          >
+            {item.link ? (
+              <a
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-500"
+              >
+                {item.text}
+              </a>
+            ) : (
+              item.text
+            )}
+          </p>
+        ))}
       </div>
     </div>
   );
